06655442457fb6016ea8fdf067b64fdc0847da18
Bruno Abrantes <bruno@brunoabrantes.com>
2016-10-28 00:51:04 +0200

    No need for VERSION when building Dockerfile

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,7 +4,7 @@ docker login -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=pageturner/identity
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 
-docker build --build-arg VERSION=$VERSION -f Dockerfile -t $REPO:$TRAVIS_COMMIT .
+docker build -f Dockerfile -t $REPO:$TRAVIS_COMMIT .
 docker tag $REPO:$TRAVIS_COMMIT $REPO:$TAG
 docker tag $REPO:$TRAVIS_COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
 docker push $REPO
