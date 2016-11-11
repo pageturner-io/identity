e60910d89d1c86d5884dd99362a9f058f8e3f3fb
Bruno Abrantes <bruno@brunoabrantes.com>
2016-11-11 16:51:38 +0100

    Fixes failing tests

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -6,7 +6,7 @@ defmodule Identity.Factory do
   def user_factory do
     %User{
       name: "Bob Belcher",
-      email: "email@example.com",
+      email: sequence(:email, &"email-#{&1}@example.com"),
       password: "secret",
       password_confirmation: "secret",
       encrypted_password: Comeonin.Bcrypt.hashpwsalt("secret")
@@ -14,13 +14,15 @@ defmodule Identity.Factory do
   end
 
   def authorization_factory do
+    email = sequence(:email, &"email-#{&1}@example.com")
+
     %Authorization{
       provider: to_string(:github),
-      uid: "email@example.com",
+      uid: email,
       token: "token",
       refresh_token: "refresh_token",
       expires_at: Guardian.Utils.timestamp + 1000,
-      user: build(:user)
+      user: build(:user, %{email: email})
     }
   end
 end
