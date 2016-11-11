39090362f016d5ee5985bbe4346fc66f21adccf3
Bruno Abrantes <bruno@brunoabrantes.com>
2016-11-11 18:24:10 +0100

    Adds and sets up Turbolinks for fast page navigation

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,6 +12,9 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import Turbolinks from "turbolinks"
+
+Turbolinks.start()
 
 // Import local files
 //
